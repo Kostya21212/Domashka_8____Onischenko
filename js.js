21da1c187023cfd8fc67554b712cfc189f14f7fc
Onischenko Kostyantyn <Onischenko411@gmail.com>
2023-12-19 21:33:11 +0200

    Change

--- js.js
+++ js.js
@@ -1,53 +1,57 @@
 const MIN_LENGTH = 1;
-const MAX_LENGTH = 10;
+const MAX_LENGTH = 1000;
 
 function isArrayNumbersValid(userAddNum) {
-    const numbers = +userAddNum;
+    const numbers = parseInt(userAddNum, 10);
+
     if (userAddNum === null) {
         alert('Окей, дякуємо за відвідування сайту!');
         return false;
     }
+
     if (!userAddNum.trim() || numbers <= 0 || !Number.isInteger(numbers)) {
         alert('Помилка, будь ласка введіть ще раз число');
         return false;
     }
+
     if (numbers < MIN_LENGTH) {
         alert('Короткий масив');
         return false;
     }
+
     if (numbers > MAX_LENGTH) {
         alert('Занадто велика кількість чисел');
         return false;
     }
+
     return true;
 }
 
 function arrayNumber() {
-    const userAddNum = prompt('Введіть будь ласка кількість чисел, які ви будете використовувати');
+    const userAddNum = prompt('Введіть будь ласка кількість чисел та всі елементи через пробіл');
+
     if (isArrayNumbersValid(userAddNum)) {
-        const numbers = +userAddNum;
-        const userArray = [];
+        const numbers = parseInt(userAddNum, 10);
+        const userInput = prompt('Введіть всі елементи через пробіл');
+        const userArray = userInput.split(' ').map(element => Number(element));
 
-        for (let i = 0; i < numbers; i++) {
-            const userElement = prompt(`Введи ${i + 1} елемент`);
-            if (userElement === null) {
-                alert('Добре, допобачення!');
-                break;
-            }
-            userArray.push(Number(userElement));
+        if (userArray.length !== numbers || userArray.some(isNaN)) {
+            alert('Помилка. Будь ласка, введіть коректні дані для всіх елементів.');
+            return;
         }
 
-        if (userArray.length === numbers) {
-            userArray.sort((a, b) => a - b);
-            alert('Введені елементи користувача: ' + userArray.join(', '));
-            const sum = userArray.reduce((a, b) => a + b, 0);
-            alert(`Сума введених чисел: ${sum}`);
-        }
+        userArray.sort((a, b) => a - b);
+        alert('Введені елементи користувача: ' + userArray.join(', '));
+        const sum = userArray.reduce((a, b) => a + b, 0);
+        alert(`Сума введених чисел: ${sum}`);
     }
 }
 
 arrayNumber();
 
-            
-        
 
+for(let i = 10; i > 0 ;i--){
+    if(i==2)break
+    
+    console.log(i)
+}
